2a15ba4ecd91a9f15e1ee502390b8fe73768fca2
Gabz047 <gabe1612mail@gmail.com>
2023-11-29 09:28:22 -0300

    AAAAAAAAAAA

---
+++ math.js
@@ -0,0 +1,22 @@
+let valor1 = document.querySelector("#1");
+let valor2 = document.querySelector("#2");
+let valor3 = document.querySelector("#3");
+let valor4 = document.querySelector("#4");
+let valor5 = document.querySelector("#5");
+let valor6 = document.querySelector("#6");
+let valor7 = document.querySelector("#7");
+let valor8 = document.querySelector("#8");
+let valor9 = document.querySelector("#9");
+let valor0 = document.querySelector("#0");
+let virgula = document.querySelector("#virgula");
+let divisao = document.querySelector("#divisao");
+let multi = document.querySelector("#multiplicacao");
+let sub = document.querySelector("#subtracao");
+let add = document.querySelector("#adicao");
+let limpar = document.querySelector("#ac");
+let resultP = document.querySelector("#previous");
+
+
+function add_num(num) {
+    document.getElementById("atual").innerHTML = num
+}x
